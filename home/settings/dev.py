dedb4d3b78e1816611276e0b0f91e5ffb7061e1e
rohitgeddam <rohitgeddam2018@gmail.com>
2022-02-04 15:16:41 +0530

    add runtime.txt

--- home/settings/dev.py
+++ home/settings/dev.py
@@ -21,3 +21,6 @@ CORS_ORIGIN_WHITELIST = (
 STRIPE_PUBLISH_KEY = 'pk_test_51KJiJWSHLi0vJZWhEMxERjgJZmYh84cFgzIqq5cxBvo2o4RSbYsaQ4I6ytl34bmXH9c6uQoIuaQPR97WX5fhAdXC00JGlWvnCh'
 STRIPE_SECRET_KEY = 'sk_test_51KJiJWSHLi0vJZWhVNoBBAUmwUPuaOhBzi6PxlTnApahm7CxvUNnvNpyNRyu39EpWH24VYLVcP2mEwYrbSqu2Fmm006rkVJqLR'
 CORS_ORIGIN_ALLOW_ALL = True
+
+import django_heroku
+django_heroku.settings(locals())
