a1d474e53e3459d0530639eb86010b1522ec206e
rohitgeddam <rohitgeddam2018@gmail.com>
2022-02-04 17:20:55 +0530

    dev2

--- home/settings/base.py
+++ home/settings/base.py
@@ -64,9 +64,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
-STATICFILES_DIRS = []
-STATIC_ROOT = os.path.join(BASE_DIR, 'build', 'static')
-MEDIA_ROOT = os.path.join(BASE_DIR, 'build', 'media')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'build/static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 SITE_ID = 1
 
 REST_FRAMEWORK = {
